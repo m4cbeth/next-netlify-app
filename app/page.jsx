0402681651c2caaed0bb144cea109cb69f42793c
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-01-04 04:03:32 -0700

    added shadcn, i hate css

--- app/page.jsx
+++ app/page.jsx
@@ -31,3 +31,7 @@ export default function Page({ children }) {
         </div>
     )
 }
+
+function makeTylerCard() {
+    
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,29 +3,72 @@ const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+    darkMode: ['class'],
     content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {
-            backgroundImage: {
-                // 'grid-pattern': "linear-gradient(to bottom, theme('colors.neutral.950 / 100%'), theme('colors.neutral.950 / 0%')), url('/images/noise.png')"
-            },
-            colors: {
-                neutral: colors.neutral
-            },
-            fontFamily: {
-                sans: ['Inter', ...defaultTheme.fontFamily.sans]
-            }
-            // #1B263B // colors to make a pallette out of
-            // #415A77
-            // #778DA9
-            // #E0E1DD
-        }
+    	extend: {
+    		backgroundImage: {},
+    		colors: {
+    			neutral: 'colors.neutral',
+    			background: 'hsl(var(--background))',
+    			foreground: 'hsl(var(--foreground))',
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
+    			},
+    			primary: {
+    				DEFAULT: 'hsl(var(--primary))',
+    				foreground: 'hsl(var(--primary-foreground))'
+    			},
+    			secondary: {
+    				DEFAULT: 'hsl(var(--secondary))',
+    				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			muted: {
+    				DEFAULT: 'hsl(var(--muted))',
+    				foreground: 'hsl(var(--muted-foreground))'
+    			},
+    			accent: {
+    				DEFAULT: 'hsl(var(--accent))',
+    				foreground: 'hsl(var(--accent-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
+    			},
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			chart: {
+    				'1': 'hsl(var(--chart-1))',
+    				'2': 'hsl(var(--chart-2))',
+    				'3': 'hsl(var(--chart-3))',
+    				'4': 'hsl(var(--chart-4))',
+    				'5': 'hsl(var(--chart-5))'
+    			}
+    		},
+    		fontFamily: {
+    			sans: [
+    				'Inter',
+                    ...defaultTheme.fontFamily.sans
+                ]
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
     },
     daisyui: {
         themes: [
             {
                 sunset: {
-                    ...require('daisyui/src/theming/themes')['sunset'],
+                    ...require('daisyui/src/theming/themes')['sunset']
                     
                 }
             }
@@ -33,5 +76,8 @@ module.exports = {
     },
     plugins: [
         require('daisyui'),
-    ]
+        require('@tailwindcss-animate'),
+        addVariablesForColors,
+        require("tailwindcss-animate")
+    ],
 };
