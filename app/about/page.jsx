8494af8fa9c40eb74739878e243b7e568e038ab3
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-01-04 13:31:44 -0700

    done with react ecosystem for now

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function AboutPage() {
+    return (
+        <div className="flex flex-">
+            <h1>You&apos;ve reached the about page.</h1>
+            
+        </div>
+    );
+}
