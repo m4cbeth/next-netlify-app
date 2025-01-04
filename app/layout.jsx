a47c9a58bac044be7dfb0de9c2c3c4ee8e0f8bbf
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-01-04 01:55:28 -0700

    finished for the night on front end

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,20 +9,16 @@ export default function RootLayout({ children }) {
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
             <body className="h-screen">
-                <main class='flex flex-col h-full w-full'>
+                <main className='flex flex-col h-full w-full'>
                     <div className="CONTAINER flex flex-row flex-1">
-                        <div className="SIDEBAR flex flex-col justify-between w-16 bg-slate-950">
-                            {/* <ul>
-                                <li>1</li>
-                                <li>2</li>
-                                <li>3</li>
-                                <li>3</li>
-                            </ul> */}
+                        <div className="SIDEBAR flex flex-col justify-between w-16 bg-slate-950 px-5">
                             <Sidenav />
-                            <div className='SETTINGSCOG flex flex-row align-middle justify-center p-2 mb-3'><FaCog size={25}/></div>
+                            <div className='flex flex-row align-middle justify-center p-2 mb-3 relative'>
+                                <FaCog className='absolute top-[-10px]' size={25}/>
+                            </div>
                         </div>
                         <div className="MAINDISPLAY pt-20 flex flex-col w-full">
-                            <div className="flex-1 border mx-10">
+                            <div className="flex-1 mx-10">
                                 {children}
                             </div>
                             <div className="flex-none">
--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,8 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
+import Image from 'next/image';
+import { Card } from 'components/card';
 
 
 export default function Page({ children }) {
@@ -13,12 +15,19 @@ export default function Page({ children }) {
         });
     }, []);
     return (
-        <div className="container mx-auto px-4 justify-end">
-            <h1 data-aos="fade-right" className="text-9xl font-bold text-right">
-                Hi!
-            </h1>
-            <h2 data-aos="fade-left" className="text-4xl font-bold text-right">
-                I'm Jaren
-            </h2>
+        <div className="container flex flex-row justify-between mx-auto px-4">
+            <div>
+                <h1 data-aos="fade-right" className="text-9xl mb-0 font-bold">
+                    Hi!
+                </h1>
+                <h2 data-aos="fade-left" className="text-4xl font-bold">
+                    I'm Jaren
+                </h2>
+            </div>
+            <Card />
+            <div>
+                <Image width="200" height="200" src="/images/jaren.jpg" alt="Jaren" className="rounded-full" /> 
+            </div>
         </div>
-)}
+    )
+}
