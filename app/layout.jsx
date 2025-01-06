bfae8f0ebab233165eeb75aadfa7628142873f21
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-01-06 09:54:22 -0700

    added theme provider and fixed font color of hero

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,15 @@
 'use client';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import { createContext, useEffect } from 'react';
 import '/styles/globals.css'
 import { useState } from 'react'
 import { Switch } from '/components/ui/switch'
 import { FaMoon, FaSun } from 'react-icons/fa'
 import Link from 'next/link'
+import { useContext } from 'react';
+
+export const ThemeContext = createContext()
 
 export default function Rootlayout({ children }) {
 
@@ -25,51 +28,52 @@ export default function Rootlayout({ children }) {
     }, []);
     return (
         <html lang="en" data-theme={themes[theme]}>
-            <head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link rel="icon" href="/favicon.svg" sizes="any" />
-            </head>
-            <body className="h-screen">                
-                <div className='flex gap-3 items-center justify-between p-1 md:p-4'>
-                    <div className='flex m-2'>
-                        <Link className='no-underline  pr-5' href="/">
-                            <div className="font-extralight text-3xl hover:scale-150 hover:text-secondary transition ease-out">
-                                JW
+            <ThemeContext.Provider value={theme}>
+                <head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <link rel="icon" href="/favicon.svg" sizes="any" />
+                </head>
+                <body className="h-screen">
+                    <div className='flex gap-3 items-center justify-between p-1 md:p-4'>
+                        <div className='flex m-2'>
+                            <Link className='no-underline  pr-5' href="/">
+                                <div className="font-extralight text-3xl hover:scale-150 hover:text-secondary transition ease-out">
+                                    JW
+                                </div>
+                            </Link>
+                            <div className="flex flex-col justify-center text-sm md:text-lg">
+                                <div className="inline-block">
+                                          <Link href="/"        className="hover:text-accent">HOME</Link > &nbsp;|
+                                    &nbsp;<Link href="/about"   className="hover:text-accent">ABOUT</Link > &nbsp;|
+                                    &nbsp;<Link href="/contact" className="hover:text-accent">CONTACT</Link >
+                                </div>
                             </div>
-                        </Link>
-                        <div className="flex flex-col justify-center text-sm md:text-lg">
-                            <div className="inline-block">
-                                      <Link href="/"        className="hover:text-accent">HOME</Link > &nbsp;|
-                                &nbsp;<Link href="/about"   className="hover:text-accent">ABOUT</Link > &nbsp;|
-                                &nbsp;<Link href="/contact" className="hover:text-accent">CONTACT</Link >
+                        </div>
+                        <div className="flex items-center gap-2 scale-75">
+                            {
+                                theme === 0 ?
+                                <FaSun data-aos="fade-down" className='text-3xl hover:cursor-pointer' onClick={() => setSynth()} />:
+                                <FaMoon data-aos="fade-up" className='text-3xl hover:cursor-pointer' onClick={() => setSynth()} />
+                            }
+                            <div className="">
+                                <Switch defaultChecked="true" onCheckedChange={() => changeTheme()} />
                             </div>
                         </div>
                     </div>
-                    <div className="flex items-center gap-2 scale-75">
-                        {
-                            theme === 0 ?
-                            <FaSun data-aos="fade-down" className='text-3xl hover:cursor-pointer' onClick={() => setSynth()} />:
-                            <FaMoon data-aos="fade-up" className='text-3xl hover:cursor-pointer' onClick={() => setSynth()} />
-                        }
-                        <div className="">
-                            <Switch defaultChecked="true" onCheckedChange={() => changeTheme()} />
-                        </div>
-                    </div>
-                </div>
-                {/* <div className="flex items-center justify-between w-full p-4">
-                    <div className="flex-none">X</div>
-                    <nav className="flex-grow text-center">
-                        <ul className="flex justify-center space-x-4">
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                        </ul>
-                    </nav>
-                    <div className="flex-none border">DIV</div>
-                </div> */}
-
-                <div className="">{children}</div>
-            </body>
+                    {/* <div className="flex items-center justify-between w-full p-4">
+                        <div className="flex-none">X</div>
+                        <nav className="flex-grow text-center">
+                            <ul className="flex justify-center space-x-4">
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                            </ul>
+                        </nav>
+                        <div className="flex-none border">DIV</div>
+                    </div> */}
+                    <div className="">{children}</div>
+                </body>
+            </ThemeContext.Provider>
         </html>
     )
 }
--- components/made-components/hero-section.jsx
+++ components/made-components/hero-section.jsx
@@ -1,8 +1,11 @@
 "use client";
 import { motion } from "framer-motion";
 import { HeroHighlight, Highlight } from "/components/ui/hero-highlight";
+import { useContext } from "react";
+import { ThemeContext } from "@/app/layout";
 
 export default function HeroSection() {
+  const theme = useContext(ThemeContext)
   return (
     (<HeroHighlight>
       <motion.h1
@@ -18,7 +21,7 @@ export default function HeroSection() {
           duration: 0.5,
           ease: [0.4, 0.0, 0.2, 1],
         }}
-        className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto ">
+        className={`text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-white max-w-4xl leading-relaxed ${ !theme ? "text-slate-900" : "text-slate-100"} lg:leading-snug text-center mx-auto`}>
         With insomnia, nothing&apos;s real. <br />
         Everything is far away. <br />
         Everything is a<br/>
