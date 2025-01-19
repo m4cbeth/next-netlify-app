03ec99f54faa217804dd1bbc2009a3d265f88323
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2025-01-18 23:06:36 -0700

    changed apos and quots

--- components/made-components/portfolio-accordian.jsx
+++ components/made-components/portfolio-accordian.jsx
@@ -56,27 +56,47 @@ export default function PortAcc() {
 }
 
 
-const jsBody = () => `
-${stars()}Oh JS, my first love. Back in high school once I had ingested HTML primer, 
-I started addeding all sorts of easter eggs and annoying things to my websites. Ohhh Javascript.`
-const expressBody = () => `
-app.post, writing up routers. Express is a great time, a
-great time by everyone, i'm sure.
-Anyways LIke iw asy ssaying.`
+const jsBody = () => (
+<div>
+    {stars(4.5)}
+    Oh JS, my first love. Back in high school once I had ingested HTML primer, 
+    I started addeding all sorts of easter eggs and annoying things to my websites. Ohhh Javascript.
+</div>
+)
+
+const expressBody = () => (
+<div>
+    {stars(4)}
+    I actually know
+</div>
+)
 const postgresBody = () => (
 <div>    
-    {stars(4)}
-    One of the newer things I've learned, ironically it being the oldest. And I'm certain I'm at the Duning Kruger
-    phase of learning, hence the low rating, because although I feel very comfortable and have worked my way around with it,
-    I know there's a depth I've yet to experience. I'm looking forward to future opprotunities to expand my skills here!
-    I'm finally coming to see the wisdom of relational databases (oops forgot I had this line here, work this in earlier.
+    {stars(2.5)}
+    <span>
+        One of the newer things I&apos;ve learned, ironically it being the oldest. And I&apos;m certain I&apos;m at the Duning Kruger
+        phase of learning, hence the low rating, because although I feel very comfortable and have worked my way around with it,
+        I know there&apos;s a depth I&apos;ve yet to experience. I&apos;m looking forward to future opprotunities to expand my skills here!
+        I&apos;m finally coming to see the wisdom of relational databases (oops forgot I had this line here, work this in earlier.
+    </span>
 </div>)
 const fireBody = () => (<div>
-    {stars(5)}
-    Firebase. Firebase is too good. Firebase fueled my imposter syndrome for about two years longer than I needed.
-    Here I was, making full stack apps, but it felt like I was cheating. I felt like I didn't know auth, I didn't know databases,
-    and, granted, at the time I didn't, but I couldn't see the forest for the trees.
-    Sometimes the stack doesn't matter if the project kicks the trick.`
+    {stars(5)}    
+    <p>
+        Firebase. Firebase just clicks for me. But Firebase also fueled my imposter syndrome for about two years longer than I needed.
+        Here I was, making full stack apps, but it felt like I was cheating. I felt like I didn&apos;t know auth, I didn&apos;t know databases,
+        I didn&apos;t know how to &quot;structure&quot; anything, I didn&apos;t know if anything I was doing was &quot;front end&quot; or &quot;back end&quot;. It just felt
+        like a cool system where you could apply JS to make things work the way you wanted.
+    </p>
+
+    <p>
+        And, granted, at the time I didn&apos;t, but I couldn&apos;t see the forest for the trees. I didn&apos;t see that I was learning, and that
+        I could credibly call myself a &quot;full stack developer&quot; even if I didn&apos;t know Kubernetes.
+    </p>
+
+    <p>
+        Sometimes the stack doesn&apos;t matter if the project kicks the trick.
+    </p>
 </div>)
 
 const stars = (numOfStars = 1) => (
